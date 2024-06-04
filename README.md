2d9dcd3f4ec46fcd47ddc4c68fd3012fbc512f1a
sebastianjkern <sebastianjakobkern@gmail.com>
2024-06-04 15:24:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Cochlear implant simulator
+
+This signal processing script tries to emulate the changes that audio signals go trough by adding 22 band pass filters together, as used in cochlear implants.
+
+Somehow the changes are not really significant from a listening experience. 
+
+The script DOES NOT simulate the other filtering and amplification that the audio signals will go trough in the ear.
---
+++ app.py
@@ -0,0 +1,38 @@
+# %%
+
+import numpy as np
+from scipy.io import wavfile
+from scipy.signal import butter, lfilter
+
+def bandpass_filter(data, lowcut, highcut, fs, order=5):
+    nyquist = 0.5 * fs
+    low = lowcut / nyquist
+    high = highcut / nyquist
+    b, a = butter(order, [low, high], btype='band', analog=True)
+    y = lfilter(b, a, data)
+    return y
+
+average_frequencies = [
+    251, 376, 501, 626, 751, 876, 1001, 1126, 1251, 1376,
+    1501, 1626, 1751, 1876, 2001, 2126, 2251, 2376, 2501,
+    2626, 2751, 2876
+]
+
+bandwidth = 1
+
+input_filename = 'abba.wav'
+output_filename = 'filtered.wav'
+fs, data = wavfile.read(input_filename)
+
+if len(data.shape) == 2:
+    data = np.mean(data, axis=1)
+
+filtered_signal = np.zeros_like(data, dtype=np.float64)
+for freq in average_frequencies:
+    lowcut = freq - bandwidth / 2
+    highcut = freq + bandwidth / 2
+    filtered_signal += bandpass_filter(data, lowcut, highcut, fs)
+
+filtered_signal = np.int16(filtered_signal / np.max(np.abs(filtered_signal)) * 32767)
+
+wavfile.write(output_filename, fs, filtered_signal)
